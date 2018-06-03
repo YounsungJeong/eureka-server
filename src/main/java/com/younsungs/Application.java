46c2047bcb3283cf95f4e1109a626411d9b0eea2
younsung <younsungs@daum.net>
2018-06-03 14:47:18 +0900

    유레카 서버 기본 설정

--- src/main/java/com/younsungs/Application.java
+++ src/main/java/com/younsungs/Application.java
@@ -1,12 +1,27 @@
 package com.younsungs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Application {
+    @Value("name")
+    private String name;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
+
+    @PostConstruct
+    public void test(){
+        System.out.println("test: " + name);
+    }
+
+
 }
